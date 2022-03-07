8dd488543e8a2274a0df92e211ba73f2e07129a1
toki <k019c1408@g.neec.ac.jp>
2022-03-07 22:23:16 +0900

    2022/03/07勉強

---
+++ Flask_pc1/app/app.py
@@ -0,0 +1,23 @@
+#Flaskとrender_template（HTMLを表示させるための関数）をインポート
+from flask import Flask,render_template,request
+
+#Flaskオブジェクトの生成
+app = Flask(__name__)
+
+
+#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
+@app.route("/")
+
+
+
+#「/index」へアクセスがあった場合に、「index.html」を返す
+@app.route("/index")
+def index():
+    name = request.args.get("name")
+    mazical = ["バナナと言ったら","黄色","黄色といったら","バナナ"]
+    return render_template("index.html",name=name,mazical=mazical)
+
+
+#おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
