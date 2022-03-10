a664d3ef8a46972cb294cc6457f65f568c8f47f6
Yumi <k019c1409@g.neec.ac.jp>
2022-03-10 22:12:19 +0900

    3月10日勉強分

--- flask/app/models/datebase.py
+++ flask/app/models/datebase.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+# 使ってるタイプをインポート
+from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
-import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
-# database.py と同じパスに、onegai.db というファイルを絶対パスで定義
-database_file = os.path.join(os.path.abspath(os.path.dirnamt(__file__)), 'onegai.db')
-engine = create_engine('sqlite:///')
+# DBを構築
+engine = create_engine('mysql+pymysql://')
