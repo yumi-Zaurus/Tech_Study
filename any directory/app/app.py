c70df53d436dddbdd2011fc5731166690feb72ed
runa <k019c1078@g.neec.ac.jp>
2022-03-16 21:53:52 +0900

    20220316

--- any directory/app/app.py
+++ any directory/app/app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
@@ -10,6 +11,11 @@ def index():
     okyo = ["色不異空","空不異色","色即是空","空即是色"]
     return render_template("index.html",name=name,okyo=okyo)
 
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    okyo = ["色不異空","色即是空","空即是色"]
+    return render_template("index.html, name=name, okyo=okyo")
 
 if __name__ == "__main__":
     app.run(debug=True)
